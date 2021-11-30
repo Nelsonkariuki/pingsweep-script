30ef5a73345aad58b196b249616d1ffd60914d0c
Nelsonkariuki <56246437+Nelsonkariuki@users.noreply.github.com>
2021-11-30 14:42:54 +0300

    final pingsweep

--- pingsweep.sh
+++ pingsweep.sh
@@ -1,9 +1,9 @@
-#! bin/bash
+#! /bin/bash
 #Simple Pingsweep script
-echo "Please Enter The Subnet"
+echo "Please Enter The Subnet:"
 read Subnet
-<<<<<<< HEAD
-if IP in $(seq 1 254);do
-ping 
-=======
->>>>>>> ec0fbe788fad8d715e51772dd8114dde1ddc416e
+
+for IP in $(seq 0 254); do
+   ping -c 1 $SUBNET.$IP
+ done
+
